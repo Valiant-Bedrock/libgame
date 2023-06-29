ed828ddd46ba661f740d1f57a1854c325798a925
Matthew Jordan <mattjordanbusiness@gmail.com>
2023-06-29 10:45:16 -0500

    feat!(`RoundState`): use native enums

--- src/libgame/game/round/RoundState.php
+++ src/libgame/game/round/RoundState.php
@@ -12,19 +12,8 @@ declare(strict_types=1);
 
 namespace libgame\game\round;
 
-use pocketmine\utils\EnumTrait;
-
-/**
- * @method static RoundState PREROUND()
- * @method static RoundState IN_ROUND()
- * @method static RoundState POSTROUND()
- */
-class RoundState {
-	use EnumTrait;
-
-	protected static function setup(): void {
-		self::register(new RoundState("preround"));
-		self::register(new RoundState("in_round"));
-		self::register(new RoundState("postround"));
-	}
+enum RoundState {
+	case PREROUND;
+	case IN_ROUND;
+	case POSTROUND;
 }
